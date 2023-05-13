5e07e6d11c85123a63166510e05e2c76c0a9c07d
sourabhbi2 <sourabhkumar1109@gmail.com>
2023-05-14 01:59:59 +0530

    docs: updated API key

--- app.js
+++ app.js
@@ -1,18 +1,27 @@
-const API_KEY = `3265874a2c77ae4a04bb96236a642d2f`
+const API_KEY = `09895e68b6b4d15d847b08075bac60ea`
 const form = document.querySelector("form")
 const search = document.querySelector("#search")
 const weather = document.querySelector("#weather")
 // const API = `https://api.openweathermap.org/data/2.5/weather?q-${city}&appid=${API_KEY}&units=metric`
 
 const getWeather = async (city) => {
-    const url = `https://api.openweathermap.org/data/2.5/weather?q-${city}&appid=${API_KEY}&units=metric`
+    const url = `https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric`
+    
     const response = await fetch(url);
-}
+    const data = await response.json();
+    console.log(data); 
 
-form.addEventListener(
-    "submit",
-    function (event) {
-        console.log(search.value)
-        event.preventDefault();
-    }
+}
+// const options = {
+//     method: 'GET',
+//     headers: {
+//         'X-RapidAPI-Key': '1caa3eb02amsh2cdb34e885b2e03p1e9b74jsn8f8662f46507',
+//         'X-RapidAPI-Host': 'weather338.p.rapidapi.com'
+//     }}
+
+form.addEventListener("submit", 
+function (event) {
+    getWeather(search.value)
+    event.preventDefault();
+}
 )
